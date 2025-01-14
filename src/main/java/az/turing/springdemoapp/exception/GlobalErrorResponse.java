520ec95785b6ae7a4f1099d3466fd42caddd3864
Behruz Tapdiqov <tapdigovbahruz@gmail.com>
2025-01-14 17:37:07 +0400

    updated

--- src/main/java/az/turing/springdemoapp/exception/GlobalErrorResponse.java
+++ src/main/java/az/turing/springdemoapp/exception/GlobalErrorResponse.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -16,36 +17,4 @@ public class GlobalErrorResponse {
     private String errorCode;
     private String message;
     private LocalDateTime timeStump;
-
-    public UUID getRequestId() {
-        return requestId;
-    }
-
-    public void setRequestId(UUID requestId) {
-        this.requestId = requestId;
-    }
-
-    public String getErrorCode() {
-        return errorCode;
-    }
-
-    public void setErrorCode(String errorCode) {
-        this.errorCode = errorCode;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public LocalDateTime getTimeStump() {
-        return timeStump;
-    }
-
-    public void setTimeStump(LocalDateTime timeStump) {
-        this.timeStump = timeStump;
-    }
 }
